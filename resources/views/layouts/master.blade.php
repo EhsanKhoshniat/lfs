5179d163448a8560eb7e6540da3172ae3898f448
Ehsan <ehsan.k93@gmail.com>
2017-09-06 10:36:01 +0430

    Create master layout file.

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+   
+
+    <title>Blog Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ URL::asset('css/blog.css') }}" rel="stylesheet">
+  </head>
+
+  <body>
+
+@include ('layouts.nav')
+
+  <div class="container">
+      <div class="row">
+            @yield ('content')
+            @include('layouts.sidebar')
+      </div>
+  </div>
+
+
+  @include ('layouts.footer')
+  </body>
+</html>
