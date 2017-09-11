c9765389703083033ba9a0aa92720256d50312c1
Ehsan <ehsan.k93@gmail.com>
2017-09-11 16:52:11 +0430

    Uncomment show route.

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/contact', 'TasksController@contact');
 Route::get('/posts', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::post('/posts', 'PostController@store'); #using word "store" is a convention
-// Route::get('/posts/{post}', 'PostController@show');
+Route::get('/posts/{post}', 'PostController@show');
 
 // Route::get('/', function (){
 // 	// $tasks = DB::table('tasks')->get();
