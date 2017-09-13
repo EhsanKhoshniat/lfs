0dad12a7fa6773145b258a7540d161e7d08ee1ca
Ehsan <ehsan.k93@gmail.com>
2017-09-13 19:39:02 +0430

    Show the post related to the comment

--- app/Comment.php
+++ app/Comment.php
@@ -4,5 +4,9 @@ namespace App;
 
 class Comment extends Model
 {
-    //
+    // Show the post related to the comment
+    public function post()
+    {
+    	return $this->belongsTo(Post::class);
+    }
 }
