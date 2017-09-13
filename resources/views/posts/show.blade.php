076e77041114644cf147a24b74d616b61d996a25
Ehsan <ehsan.k93@gmail.com>
2017-09-13 19:40:33 +0430

    Create comments section on singe post.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,7 +7,19 @@
             <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString()}}</p>
             {{ $post->body }}
           </div><!-- /.blog-post -->
- 
+ 		<hr>
+ 		<div class="comments">
+ 		<ul class="list-group">
+ 			@foreach ($post->comments as $comment)
+ 				<li class="list-group-item">
+ 				<strong>
+ 				{{ $comment->created_at->diffForHumans() }}:
+ 				</strong>
+ 				{{ $comment->body }}
+ 				</li>
+ 			@endforeach
+ 		</ul>
+ 		</div>
 </div>
 @endsection
 
