30d7e4d5a6e851bb5effdb3f6fa8f7cbedd882f8
Ehsan <ehsan.k93@gmail.com>
2017-09-13 19:40:04 +0430

    Show related comments of a post.

--- app/Post.php
+++ app/Post.php
@@ -5,4 +5,8 @@ namespace App;
 class Post extends Model
 {
     // protected $fillable = ['title', 'body']; // Use fillable proprty as a white list.
+    public function comments()
+    {
+    	return $this->hasMany(Comment::class);
+    }
 }
