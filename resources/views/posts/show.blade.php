6f7aa88b16a7a673d0c203a6bedb724923b574b3
Ehsan <ehsan.k93@gmail.com>
2017-09-14 14:00:57 +0430

    Create add comment form.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -20,6 +20,21 @@
  			@endforeach
  		</ul>
  		</div>
+ 		{{-- Add a comment --}}
+ 		<hr>
+ 		<div class="card">
+ 			<div class="card-block">
+ 				<form method="POST" action="/posts/{{ $post->id }}/comments">
+ 				{!! csrf_field() !!}
+ 					<div class="form-group">
+ 						<textarea name="body" placeholder="Your Comment Here" class="form-control"></textarea>
+ 					</div>
+ 					  <div class="form-group">
+					    <button type="submit" class="btn btn-primary">Add Comment</button>
+					  </div>
+ 				</form>
+ 			</div>
+ 		</div>
 </div>
 @endsection
 
