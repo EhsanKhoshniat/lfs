5d5140afacc3c555ba7686c3001064e0d4fe37dd
Ehsan <ehsan.k93@gmail.com>
2017-09-14 14:02:30 +0430

    Create comments controller.

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use App\Comment;
+
+class CommentsController extends Controller
+{
+    public function store(Post $post)
+    {
+    // Long way:
+    	// Comment::create([
+    	// 	'body' => request('body'),
+    	// 	'post_id' => $post->id
+    	// 	]);
+
+    // Elequent way:
+    	$post->addComment(request('body'));
+
+    	return back();
+    }
+}
