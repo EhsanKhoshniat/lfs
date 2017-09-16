027d421b06c0fb60a81526ce9b1568a80fc0d792
Ehsan <ehsan.k93@gmail.com>
2017-09-16 11:41:27 +0430

    Add register and login routes.

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,16 @@ Route::get('/tasks/{task}', 'TasksController@show');
 Route::get('/', 'TasksController@welcome');
 Route::get('/about', 'TasksController@about');
 Route::get('/contact', 'TasksController@contact');
-Route::get('/posts', 'PostController@index');
+Route::get('/posts', 'PostController@index')->name('home');
 Route::get('/posts/create', 'PostController@create');
 Route::post('/posts', 'PostController@store'); #using word "store" is a convention
 Route::get('/posts/{post}', 'PostController@show');
 
 Route::post('/posts/{post}/comments', 'CommentsController@store');
+
+Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
+Route::get('/login','SessionsController@create');
 
 // Route::get('/', function (){
 // 	// $tasks = DB::table('tasks')->get();
