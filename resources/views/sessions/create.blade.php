4b4adafe6c9bb2166f9370c3f8c8358d183432e2
Ehsan <ehsan.k93@gmail.com>
2017-09-16 17:06:08 +0430

    Create sessions views.

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,28 @@
+@extends ('layouts.master')
+
+@section ('content')
+	<div class="col-sm-8">
+		<h1>Log in</h1>
+
+		<form method="POST" action="/login">
+			{{ csrf_field() }}
+
+			<div class="form-group">
+				<label for="email">Email:</label>
+				<input type="email" name="email" id="email" class="form-control" required>
+			</div>
+
+			<div class="form-group">
+				<label for="password">Password:</label>
+				<input type="password" name="password" id="password" class="form-control" required>
+			</div>
+			
+			<div class="form-group">
+				<button type="submit" class="btn btn-primary">Login</button>
+			</div>
+
+			@include ('layouts.errors')
+
+		</form>
+	</div>
+@endsection
