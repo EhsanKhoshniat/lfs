9d233a28cd932fecdfe5126bf6510f335a8deef9
Ehsan <ehsan.k93@gmail.com>
2017-09-19 12:23:47 +0430

    Create filter month and year query scope on Post mode;.

--- app/Post.php
+++ app/Post.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App;
+use Carbon\Carbon;
 
 class Post extends Model
 {
@@ -18,5 +19,19 @@ class Post extends Model
     public function addComment($body)
     {
     	$this->comments()->create(compact('body'));
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+        if ($month = $filters['month']) 
+        {
+            $query->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if ($year = $filters['year'])
+        {
+            $query->whereYear('created_at', $year);
+        }
+
     }
 }
