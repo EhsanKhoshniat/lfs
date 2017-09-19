370ca609b48254a48ebb3007b8031d2ea1a08366
Ehsan <ehsan.k93@gmail.com>
2017-09-19 12:24:59 +0430

    Define login view.

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::post('/posts/{post}/comments', 'CommentsController@store');
 Route::get('/register', 'RegistrationController@create');
 Route::post('/register', 'RegistrationController@store');
 
-Route::get('/login','SessionsController@create');
+Route::get('/login','SessionsController@create')->name('login');
 Route::post('/login', 'SessionsController@store');
 Route::get('/logout', 'SessionsController@destroy'); // Post request is better choice for this
 
