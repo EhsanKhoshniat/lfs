b22d59eda3a31ede3061db244d66c42ce4104396
Ehsan <ehsan.k93@gmail.com>
2017-09-25 20:42:19 +0330

    Create RegistrationForm.

---
+++ app/Http/Requests/RegistrationForm.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\User;
+use App\Mail\Welcome;
+use Illuminate\Support\Facades\Mail;
+
+class RegistrationForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|unique:users|email',
+            'password' => 'required|confirmed'
+        ];
+    }
+
+    public function persist()
+    {
+        //Create and save the user
+        $user = User::create([ 
+        'name' => $this->name,
+        'email' => $this->email,
+        'password' => bcrypt($this->password)
+        ]);
+
+        //Sign them up
+        auth()->login($user);
+
+        Mail::to($user)->send(new Welcome($user));
+    }
+}
