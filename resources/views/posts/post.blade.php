5767ebe789d29191a3cf708b605c23b3f776ee13
Ehsan <ehsan.k93@gmail.com>
2017-09-28 11:08:54 +0330

    Add Tags to post meta.

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -2,6 +2,10 @@
             <a href="posts/{{ $post->id }}">
             <h2 class="blog-post-title">{{ $post->title}}</h2>
             </a>
-            <p class="blog-post-meta">{{ $post->user->name }} on {{ $post->created_at->toFormattedDateString()}}</p>
+            <p class="blog-post-meta">{{ $post->user->name }} on {{ $post->created_at->toFormattedDateString()}}
+            @foreach ($post->tags as $tag)
+            	<a href="/posts/tags/{{ $tag->name }}">{{ $tag->name }}</a>
+            @endforeach
+            </p>
             {{ $post->body }}
           </div><!-- /.blog-post -->
