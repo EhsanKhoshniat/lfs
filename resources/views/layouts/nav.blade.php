e9db40d4b5085a97b030be29e7e068b8ca51bc7b
Ehsan <ehsan.k93@gmail.com>
2017-11-01 10:47:19 +0330

    Add user profile menu.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -8,7 +8,23 @@
           <a class="nav-link" href="#">About</a>
           
           @if (Auth::check())
-          <a class="nav-link ml-auto" href="#">{{ auth()->user()->name }}</a>
+            <div class="dropdwon navbar-nav flex-row ml-md-auto d-none d-md-flex ">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                   {{ auth()->user()->name }}
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">Profile</a>
+              <a class="dropdown-item" href="#">Change Password</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="/logout">Logout</a>
+            </div>
+            </div>
+          @else
+        <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+            <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
+            &nbsp;
+            <li class="nav-item"><a class="nav-link" href="/register">Register</a></li>
+        </ul>
           @endif
           
         </nav>
