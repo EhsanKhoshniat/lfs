76aabb54446272f550279420fa6d51e30872c28d
Ehsan <ehsan.k93@gmail.com>
2017-11-01 10:50:38 +0330

    Create dashboard views.

---
+++ resources/views/dashboard/head.blade.php
@@ -0,0 +1,8 @@
+<head>
+<meta charset="utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content="width=device-width, initial-scale=1" name="viewport"/>
+<meta content="" name="description"/>
+<meta content="" name="author"/>
+	<title>Dashboard</title>
+</head>
---
+++ resources/views/dashboard/master.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+@include ('dashboard.head')
+<body>
+
+</body>
+</html>
